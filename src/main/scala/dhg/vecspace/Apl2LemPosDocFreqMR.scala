1005dacc56648ec9fa30206aa8e3da62037c62c2
Dan Garrette <dhgarrette@gmail.com>
2013-07-27 20:24:40 -0500

    fixed some bugs

--- src/main/scala/dhg/vecspace/Apl2LemPosDocFreqMR.scala
+++ src/main/scala/dhg/vecspace/Apl2LemPosDocFreqMR.scala
@@ -25,16 +25,48 @@ class Apl2LemPosDocFreqMRClass(args: Args) extends Job(args) {
     .map(_.trim)
     .filter(_.nonEmpty)
     .flatMap { articleLine =>
-      val Vector(id, typ, paragraphs @ _*) = articleLine.split("\t").toVector
-      paragraphs.toSet.par // Using a SET here means at most one count per article
-        .flatMap((paragraph: String) =>
-          Giga2TokLemPos.annotator.apply(paragraph)
-            .flatMap(_.collect {
-              case AnnotatedToken(w, CleanTok(l), CleanPos(p), _) if Idf.ValidLemma(l) && !Idf.InvalidPos(p) => ("%s\t%s".format(l, p), 1)
-            })).seq
+        val Vector(id, typ, paragraphs @ _*) = articleLine.split("\t").toVector
+        val sentences = paragraphs.par.flatMap((paragraph: String) =>Giga2TokLemPos.annotator.apply(paragraph))
+        val annToks = sentences.flatten
+        val validLps = annToks.collect { case AnnotatedToken(w, CleanTok(l), CleanPos(p), _)
+                  if Idf.ValidLemma(l) && !Idf.InvalidPos(p)
+                     => (l, p) }
+        val validLpSet = validLps.toSet
+        validLpSet.map { case (l,p) => "%s\t%s".format(l,p) -> 1 }.seq
     }
     .group
     .reduce(_ + _)
     .map { case (lp, c) => "%s\t%s".format(lp, c) }
     .write(TypedTsv[String](outputTlpFile))
 }
+
+object NonMRApl2LemPosDocFreq {
+  def main(args: Array[String]) {
+    val inputAplFile = "/scratch/01899/dhg1/gigaword5-apl00-10k"
+    File(inputAplFile).readLines.zipWithIndex
+      .map { case (x,i) => if(i%1000==0) println(i); x }
+      .map(_.trim)
+      .filter(_.nonEmpty)
+      .flatMap { articleLine =>
+        val Vector(id, typ, paragraphs @ _*) = articleLine.split("\t").toVector
+        val sentences = paragraphs.flatMap((paragraph: String) =>Giga2TokLemPos.annotator.apply(paragraph))
+        val annToks = sentences.flatten
+        val validLps = annToks.collect { case AnnotatedToken(w, CleanTok(l), CleanPos(p), _) 
+                  if Idf.ValidLemma(l) && !Idf.InvalidPos(p) 
+                     && Set("year","make")(l)
+                     => (l, p) }
+        val validLpSet: Set[(String,String)] = validLps.toSet
+        validLpSet.mapToVal(1)
+    }
+    .groupByKey
+    .mapVals(_.reduce(_+_))
+    .map { case (lp, c) => "%s\t%s".format(lp, c) }
+    .foreach(println)
+   
+  }
+}
+
+
+
+
+
